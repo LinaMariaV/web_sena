b04ecfacc5ff14be4a861a8c5478e592e11e5bd1
Lina Villalba <linavilla1499@gmail.com>
2024-05-25 19:49:13 -0500

    Fix users services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-const API_URL = "http://localhost:3001/api/users/"
+import axios from "axios";
+const API_URL = "http://localhost:3001/api/users/";
 
 const getAllUsers = async (token) => {
   const response = await axios.get(API_URL, {
@@ -9,36 +9,36 @@ const getAllUsers = async (token) => {
   });
 
   return response.data;
+};
 
-  const getUser = async (id) => {
-    const response = await axios.get(API_URL + id);
-    return response.data;
-  };
+const getUser = async (id) => {
+  const response = await axios.get(API_URL + id);
+  return response.data;
+};
 
-  const postUser = async (name, email, password, role) => {
-    const response = await axios.post(API_URL + "create", {
-      name,
-      email,
-      password,
-      role,
-    });
-    return response.data;
-  };
+const postUser = async (name, email, password, role) => {
+  const response = await axios.post(API_URL + "create", {
+    name,
+    email,
+    password,
+    role,
+  });
+  return response.data;
+};
 
-    const putUser = async (id, name, email, password, role) => {
-        const response = await axios.put(API_URL + id, {
-        name,
-        email,
-        password,
-        role,
-        });
-        return response.data;
-    };
+const putUser = async (id, name, email, password, role) => {
+  const response = await axios.put(API_URL + id, {
+    name,
+    email,
+    password,
+    role,
+  });
+  return response.data;
+};
 
-    const deleteUser = async (id) => {
-        const response = await axios.delete(API_URL + id);
-        return response.data;
-    };
+const deleteUser = async (id) => {
+  const response = await axios.delete(API_URL + id);
+  return response.data;
+};
 
-}
-export { getAllUsers, getUser, postUser, putUser, deleteUser}
+export { getAllUsers, getUser, postUser, putUser, deleteUser };
