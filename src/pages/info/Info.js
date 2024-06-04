1438dbb900ba90e271b1ad94c474ce43e94d132f
Lina Villalba <linavilla1499@gmail.com>
2024-06-04 18:43:06 -0500

    Create styles to page info

--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -1,50 +1,62 @@
-import Atentionsection from "./section_info/atention";
+import React from "react";
+import "./section_info/info.css";
+import { Row, Col } from "react-bootstrap";
 
 function Info() {
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Información sobre la gestion de Viajes</h1>
-      <p className="lead">
-        Bienvenido a nuestra aplicación de viajes turísticos. Aquí podrás
-        explorar una variedad de destinos y planificar tu viaje de manera
-        sencilla.
+    <div className="info-container container mt-5">
+      <h1 className="mb-4">Gestión de Viajes Turísticos</h1>
+      <p className="lead text-center">
+        Bienvenido a nuestra aplicación de viajes turísticos. Explora destinos,
+        planifica tu viaje y disfruta de una experiencia inolvidable.
       </p>
+      <Row>
+      <div className="info-section">
+        <Col>
+        <h2 className="text-center mt-5">Destinos</h2>
+        <p className="text-center">
+          Descubre una amplia variedad de destinos turísticos. Cada destino
+          incluye una breve descripción y fotos para ayudarte a elegir tu
+          próximo viaje.
+        </p>
+        </Col>
+        <Col>
+        <img
+          src="path/to/destination-image.jpg"
+          alt="Imagen de destino turístico"
+          className="img-fluid mb-5 mx-auto d-block"
+        />
+        </Col>
+      </div>
+      </Row>
 
-      <div className="row">
-        <div className="col-md-6">
-          <h2 className="mt-4">Destinos</h2>
-          <p>
-            En nuestra sección de destinos, podrás explorar una amplia gama de
-            lugares turísticos disponibles. Cada destino incluye una breve
-            descripción y foto para ayudarte a elegir tu próximo viaje.
-          </p>
-          <img alt="Imagen de destino turístico" className="img-fluid mb-3" />
-        </div>
-        <div className="col-md-6">
-          <h2 className="mt-4">Planificación del Viaje</h2>
-          <h3>Dentro de las cosas que podras gestionar estan:</h3>
-          <ul className="list-group mb-4">
-            <li className="list-group-item">Ciudad de Origen</li>
-            <li className="list-group-item">Ciudad de Destino</li>
-            <li className="list-group-item">Fecha del Vuelo</li>
-            <li className="list-group-item">Número de Tiquetes</li>
-          </ul>
-        </div>
+      <div className="info-section">
+        
+        <h2 className="text-center mt-5">Planificación del Viaje</h2>
+        <p className="text-center">
+          Gestiona todos los aspectos de tu viaje, desde la ciudad de origen
+          hasta el número de tiquetes.
+        </p>
+        <ul className="list-group mb-5">
+          <li className="list-group-item">Ciudad de Origen</li>
+          <li className="list-group-item">Ciudad de Destino</li>
+          <li className="list-group-item">Fecha del Vuelo</li>
+          <li className="list-group-item">Número de Tiquetes</li>
+        </ul>
+      </div>
+      
+
+      <div className="info-section">
+        <h2 className="text-center mt-5">Detalles de Alojamiento</h2>
+        <p className="text-center">
+          Personaliza los detalles de tu alojamiento y otros servicios
+          adicionales contactando a nuestro equipo de atención al cliente.
+        </p>
       </div>
 
-      <h2 className="mt-4">Detalles de Alojamiento</h2>
-      <p>
-        Para agregar y personalizar los detalles de tu alojamiento u otros
-        servicios adicionales, por favor contacta a nuestro equipo de atención
-        al cliente. Estaremos encantados de asistirte para crear una experiencia
-        de viaje a tu medida.
-      </p>
-
-      <Atentionsection />
-
-      <div className="faq-section">
-        <h2 className="text-center">Preguntas Frecuentes</h2>
-        <div className="faq-item">
+      <div className="faq-section mt-5">
+        <h2 className="text-center mb-4">Preguntas Frecuentes</h2>
+        <div className="faq-item mb-3">
           <h3>¿Cómo puedo reservar un viaje?</h3>
           <p>
             Puedes reservar tu viaje fácilmente a través de nuestra plataforma
@@ -52,20 +64,34 @@ function Info() {
             pasos indicados.
           </p>
         </div>
-        <div className="faq-item">
+        <div className="faq-item mb-3">
           <h3>¿Qué métodos de pago aceptan?</h3>
           <p>
             Aceptamos todas las principales tarjetas de crédito, PayPal y
             transferencias bancarias.
           </p>
         </div>
-        <div className="faq-item">
+        <div className="faq-item mb-3">
           <h3>¿Puedo cancelar o modificar mi reserva?</h3>
           <p>
             Sí, ofrecemos opciones flexibles para cancelar o modificar tu
             reserva. Consulta nuestra política de cancelación para más detalles.
           </p>
         </div>
+      </div>
+      <div className="atention-section mt-5">
+        <h2 className="text-center">Atención al Cliente</h2>
+        <ul className="list-group mb-4">
+          <li className="list-group-item">
+            <strong>Teléfono:</strong> 123-456-7890
+          </li>
+          <li className="list-group-item">
+            <strong>Correo Electrónico:</strong> contacto@viajesturisticos.com
+          </li>
+          <li className="list-group-item">
+            <strong>Chat de WhatsApp:</strong> 3176190286
+          </li>
+        </ul>
       </div>
     </div>
   );
