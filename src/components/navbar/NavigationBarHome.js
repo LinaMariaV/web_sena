d31d246454fe83ee5ab6415a54f317f32ce414a8
Lina Villalba <linavilla1499@gmail.com>
2024-06-04 01:14:32 -0500

    Add code deleted

--- src/components/navbar/NavigationBarHome.js
+++ src/components/navbar/NavigationBarHome.js
@@ -1,20 +1,14 @@
 import "./Navigation.css";
 import NavigationBar from "./NavigationBar";
 
-
 function NavigationBarHome() {
-  
   return (
     <div className="background-image">
       <NavigationBar enableTitle={false} />
-
-      <div >
-        <div className="title-home ">
+      <div>
+        <div className="title-home">
           <h1 className="zize-title">¡Planea tu proximo viaje con nosotros!</h1>
         </div>
-        
-        
-
       </div>
     </div>
   );
