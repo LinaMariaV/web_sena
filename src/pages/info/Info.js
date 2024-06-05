98ae1539b1e8b74f8a75935d6dfbbd5c32e902c3
Lina Villalba <linavilla1499@gmail.com>
2024-06-04 22:59:33 -0500

    Add styles and information text

--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -1,58 +1,68 @@
 import React from "react";
 import "./section_info/info.css";
 import { Row, Col } from "react-bootstrap";
+import imagenppal from "../../assets/imagen_principal.jpg";
 
 function Info() {
   return (
     <div className="info-container container mt-5">
-      <h1 className="mb-4">Gestión de Viajes Turísticos</h1>
+      <h1 className=" font-tittle mb-4">Gestión de Viajes Turísticos</h1>
       <p className="lead text-center">
-        Bienvenido a nuestra aplicación de viajes turísticos. Explora destinos,
-        planifica tu viaje y disfruta de una experiencia inolvidable.
+        ¡Bienvenido a nuestra aplicación de viajes turísticos. Explora destinos,
+        planifica tu viaje y disfruta de una experiencia inolvidable!
       </p>
+
       <Row>
-      <div className="info-section">
-        <Col>
-        <h2 className="text-center mt-5">Destinos</h2>
-        <p className="text-center">
-          Descubre una amplia variedad de destinos turísticos. Cada destino
-          incluye una breve descripción y fotos para ayudarte a elegir tu
-          próximo viaje.
-        </p>
+        <Col className="col justify-content-center d-flex flex-column">
+          <h2 className=" text-center ">Destinos</h2>
+          <p className="text-justify">
+            Descubre una amplia variedad de destinos turísticos. Cada destino
+            incluye una breve descripción y fotos para ayudarte a elegir tu
+            próximo viaje.
+          </p>
         </Col>
+
         <Col>
-        <img
-          src="path/to/destination-image.jpg"
-          alt="Imagen de destino turístico"
-          className="img-fluid mb-5 mx-auto d-block"
-        />
+          <img
+            src={imagenppal}
+            alt="Imagen de destino turístico"
+            className="img-fluid mb-5 mx-auto d-block"
+          />
         </Col>
-      </div>
       </Row>
 
-      <div className="info-section">
-        
-        <h2 className="text-center mt-5">Planificación del Viaje</h2>
-        <p className="text-center">
-          Gestiona todos los aspectos de tu viaje, desde la ciudad de origen
-          hasta el número de tiquetes.
-        </p>
-        <ul className="list-group mb-5">
-          <li className="list-group-item">Ciudad de Origen</li>
-          <li className="list-group-item">Ciudad de Destino</li>
-          <li className="list-group-item">Fecha del Vuelo</li>
-          <li className="list-group-item">Número de Tiquetes</li>
-        </ul>
-      </div>
-      
-
-      <div className="info-section">
-        <h2 className="text-center mt-5">Detalles de Alojamiento</h2>
-        <p className="text-center">
-          Personaliza los detalles de tu alojamiento y otros servicios
-          adicionales contactando a nuestro equipo de atención al cliente.
-        </p>
-      </div>
+      <Row>
+        <Col>
+          <div className="info-section">
+            <h2 className="text-center mt-5">Planificación del Viaje</h2>
+            <p className="text-justify">
+              Gestiona todos los aspectos de tu viaje, desde la ciudad de origen
+              hasta el número de tiquetes.
+            </p>
+            <ul className="list-group mb-5">
+              <li className="list-group-item">Ciudad de Origen</li>
+              <li className="list-group-item">Ciudad de Destino</li>
+              <li className="list-group-item">Fecha del Vuelo</li>
+              <li className="list-group-item">Número de Tiquetes</li>
+            </ul>
+          </div>
+        </Col>
+        <Col>
+          <div className="info-section">
+            <h2 className="text-center mt-5">Detalles de Alojamiento</h2>
+            <p className="text-justify">
+              Personaliza los detalles de tu alojamiento y otros servicios
+              adicionales contactando a nuestro equipo de atención al cliente.
+              Aunque en nuestra pagina web no disponemos de esta funcionalidad,
+              puedes contactarnos a través de los medios de contacto que se
+              encuentran en la parte inferior de esta página. Nuestro equipo
+              estará encantado de ayudarte. Una que hayas reservado tus vuelos
+              uno de nuestros asesores se pondrá en contacto contigo para
+              ofrecerte las mejores opciones de alojamiento.
+            </p>
+          </div>
+        </Col>
+      </Row>
 
       <div className="faq-section mt-5">
         <h2 className="text-center mb-4">Preguntas Frecuentes</h2>
