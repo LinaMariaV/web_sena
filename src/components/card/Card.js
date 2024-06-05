34baf3920955ff1dfed9402cd99eff6d39dfb967
Lina Villalba <linavilla1499@gmail.com>
2024-06-04 22:51:14 -0500

    Add feature

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,14 +2,14 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./Card.css";
 
-function CardI({ className, image, title, text, button }) {
+function CardI({ className, image, title, text, button, onClick }) {
   return (
     <Card className={className}>
       <Card.Img variant="top" src={image} className="img-card" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
-        {button ? <Button variant="primary">{button}</Button> : null}
+        {button ? <Button onClick={onClick} variant="primary">{button} </Button> : null}
       </Card.Body>
     </Card>
   );
